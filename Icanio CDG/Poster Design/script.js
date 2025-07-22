e509acb735ab3feeb05c09ad7ba5dfafcdda3f64
Rakesh S.P <sureshsuresh95176@gmail.com>
2025-07-22 10:32:16 +0530

    Version 1.0

---
+++ Icanio CDG/Poster Design/script.js
@@ -0,0 +1,6 @@
+const urlParams = new URLSearchParams(window.location.search);
+const header = urlParams.get("icanioHeader");
+const college = urlParams.get("college");
+const customCollege = urlParams.get("customCollege");
+const description = urlParams.get("description");
+const quote = urlParams.get("quote");
